b7f498708ea53c4f5473f29113be5d921f267147
anisoghomonyan92 <anuliks@yahoo.com>
2022-05-29 02:27:39 +0400

    FigurePainter1

---
+++ src/homework/FigurePainter1.java
@@ -0,0 +1,40 @@
+package homework;
+
+public class FigurePainter1 {
+    public static void main(String[] args) {
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j <=i ; j++) {
+//                System.out.print("*");
+//
+//            }
+//            System.out.println();
+//
+//        }
+        /* Figure  1*/
+//        for (int i = 5; i >0 ; i--) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//
+//            }
+//            System.out.println();
+//
+//        }
+        /*Figure 3*/
+
+
+
+            
+
+
+        
+        
+    }
+
+}
+
+
+
+
+
+
+
